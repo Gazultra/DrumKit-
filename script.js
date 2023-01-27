e26ca3cdf0b088fb447e0c115ff73e702d96bc9c
MacBook <macbook@MacBooks-MacBook-Pro.local>
2023-01-27 16:30:49 +0000

    drumkit

---
+++ script.js
@@ -0,0 +1,68 @@
+
+
+
+const db1 = document.querySelector(`.aButton`)
+        document.addEventListener(`keypress`, function(event) {
+        console.log(event)
+            if (event.code === "KeyA"){
+                const audio = document.querySelector(`audio[data-key = "druma"]`)
+                audio.play()
+             }     else if(event.code === "KeyS"){
+                const audio = document.querySelector(`audio[data-key = "drums"]`)
+                audio.play()
+             } else if(event.code === "KeyD"){
+                const audio = document.querySelector(`audio[data-key = "drumd"]`)
+                audio.play()
+             } 
+             else if(event.code === "KeyF"){
+                const audio = document.querySelector(`audio[data-key = "drumf"]`)
+                audio.play()
+             } 
+             else if(event.code === "KeyG"){
+                const audio = document.querySelector(`audio[data-key = "drumg"]`)
+                audio.play()
+             } 
+             if (event.code === "KeyH"){
+                const audio = document.querySelector(`audio[data-key = "drumh"]`)
+                audio.play()
+             }     else if(event.code === "KeyJ"){
+                const audio = document.querySelector(`audio[data-key = "drumj"]`)
+                audio.play()
+             } else if(event.code === "KeyK"){
+                const audio = document.querySelector(`audio[data-key = "drumk"]`)
+                audio.play()
+             } 
+             else if(event.code === "KeyL"){
+                const audio = document.querySelector(`audio[data-key = "druml"]`)
+                audio.play()
+             } 
+             else if(event.code === "KeyZ"){
+                const audio = document.querySelector(`audio[data-key = "drumz"]`)
+                audio.play()
+             } 
+             if (event.code === "KeyX"){
+                const audio = document.querySelector(`audio[data-key = "drumx"]`)
+                audio.play()
+             }     else if(event.code === "KeyC"){
+                const audio = document.querySelector(`audio[data-key = "drumc"]`)
+                audio.play()
+             } else if(event.code === "KeyV"){
+                const audio = document.querySelector(`audio[data-key = "drumv"]`)
+                audio.play()
+             } 
+             else if(event.code === "KeyB"){
+                const audio = document.querySelector(`audio[data-key = "drumb"]`)
+                audio.play()
+             } 
+             else if(event.code === "KeyN"){
+                const audio = document.querySelector(`audio[data-key = "drumn"]`)
+                audio.play()
+             } 
+             else if(event.code === "KeyM"){
+                const audio = document.querySelector(`audio[data-key = "drumm"]`)
+                audio.play()
+             } 
+    });
+
+
+  
